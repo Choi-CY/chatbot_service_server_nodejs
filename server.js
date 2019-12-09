224f70d7eeb912d3276b8f4c8302af2206acd13d
window server <ths0023@naver.com>
2019-12-09 15:47:33 +0900

    chatbot server modify

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ require('moment-timezone');
 
 const upload = multer({dest: './MMDB/upload'});
 //app.use('/uploads',serveStatic(path.join(__dirname, 'uploads')));
-
+/*
 const connection = mysql.createConnection({
   host: conf.host,
   user: conf.user,
@@ -23,7 +23,40 @@ const connection = mysql.createConnection({
   database: conf.database
 });
 connection.connect(); 
+*/
 
+var db_config ={
+  "host":"113.198.229.223",
+  "user":"mm_db",
+  "password":"itcloud123!@",
+  "port":"3306",
+  "datebase":"mm_db"
+}
+
+var connection;
+
+function handleDisconnect() {
+connection = mysql.createConnection(db_config); // Recreate the connection, since
+                                                // the old one cannot be reused.
+
+connection.connect(function(err) {              // The server is either down
+  if(err) {                                     // or restarting (takes a while sometimes).
+    console.log('error when connecting to db:', err);
+    setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+  }                                     // to avoid a hot loop, and to allow our node script to
+});                                     // process asynchronous requests in the meantime.
+                                        // If you're also serving http, display a 503 error.
+connection.on('error', function(err) {
+  console.log('db error', err);
+  if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+    handleDisconnect();                         // lost due to either server restart, or a
+  } else {                                      // connnection idle timeout (the wait_timeout
+    throw err;                                  // server variable configures this)
+  }
+});
+}
+
+handleDisconnect();
 
 const Bot = require('./dialog_test.js')
 //챗봇 객체 생성 
@@ -195,7 +228,24 @@ app.post('/post', (req, res) => {
             res.send({message:{text: '_'+test.substr(2,test.length) + '_'+str}});
           }
           else{
-            res.send({message:{text: result[0].queryResult.fulfillmentText,image : ""}});
+            res.send({message:{text: result[0].queryResult.fulfillmentText,image : ""}}); // 다이얼로그 플로우 답변을 안드로이드에 전송
+            let database = new DB();
+            var querys = 'INSERT INTO mm_db.history VALUES(?,?,?,?)' // 대화 내용  DB에 저장 쿼리
+            moment.tz.setDefault("Asia/Seoul");  // 날짜
+            var date = moment().format('YYYY-MM-DD HH:mm:ss');
+            // 실제 DB로 해당 데이터를 전송
+            connection.query(querys,[inputData.sessionId,date,inputData.query,result[0].queryResult.fulfillmentText],(err, rows, fields) => {
+              if(!err){
+                 //res.send(rows);
+                  var check = 'true';
+                  console.log(check)
+                  //res.send({message:{text: check}});
+              }else{
+                  var check = 'false';
+                  console.log(check)
+                   //res.send({message:{text: check}});
+              }  
+            });
             console.log(result[0].queryResult.fulfillmentText);
           }
           
@@ -208,6 +258,43 @@ app.post('/post', (req, res) => {
    var bitmap = fs.readFileSync(file);
    return new Buffer(bitmap).toString('base64');
  }
+
+// 이전 채팅 데이터 불러오기 
+ app.post('/logchecking', (req, res) => {
+  console.log('who get in here post /users');
+  let body = [];
+  var inputData;
+  var senddata;
+  req.on('data', (data) => {
+    inputData = JSON.parse(data);
+    console.log(inputData);
+  });
+
+  req.on('end', () => {
+    let database = new DB();
+    var querys = 'SELECT ask,reply FROM mm_db.history WHERE Id= \''+inputData.id+'\' ORDER BY timedate '
+    connection.query(querys,(err, rows, fields) => {
+      if(!err){
+          console.log(rows);
+          if(!rows[0]){
+            var checking = 'No'; 
+            res.send(checking);
+            console.log(checking);    
+          }
+          else{
+            res.send(rows);
+          }
+      }else{
+        console.log(err);
+      }
+    });
+  });
+});
+
+
+
+
+
  app.post('/login',(req,res) =>{
   console.log('check Login service');
   var inputData_login;
